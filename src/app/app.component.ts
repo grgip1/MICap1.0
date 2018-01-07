ea03e0a103ed66f04fc14ac8dfd4c70d94a7d293
DESKTOP-9DEVMTF\Pero Grgic <grgicpero@hotmail.com>
2018-01-07 05:47:09 +0100

    Mit Midata verbunden

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { Http, Response } from '@angular/http';
 import { Component } from '@angular/core';
+import 'rxjs/add/operator/map';
+import { Authent } from './authent';
+//import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  CONNECTION_STATE: boolean;
+  AUTO_LOGIN: boolean;
+  authtoken: string;
+
+  credentials = (
+    {
+      appname: 'MICap',
+      device: 'debug',
+      secret: 'livingcase2',
+      username: '',
+      password: '',
+      role: 'RESEARCH'
+    }
+  );
+
+
+constructor(private http: Http, /*private _router: Router*/) { }
+
+
+  login() {
+    return this.http.post('https://test.midata.coop/v1/auth', this.credentials).map(
+      (response) => response.json()).subscribe(
+      (data) => console.log(data),
+      );
+
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
