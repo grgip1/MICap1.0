364505bafb7d02c7f5b7c2deac6645ee9fe69bed
Pero Grgic <pero.grgic@students.bfh.ch>
2018-01-09 18:07:48 +0100

    Login angepasst

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Http, Response } from '@angular/http';
 import { Component } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { Authent } from './authent';
-//import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
@@ -32,11 +30,19 @@ export class AppComponent {
 
 constructor(private http: Http, /*private _router: Router*/) { }
 
+setUsername (uname: string) {
+  this.username = uname;
+}
+
+setPassword ( passw: string) {
+  this.password = passw;
+}
 
   login(uname: string, passw: string) {
 
     this.username = uname;
     this.password = passw;
+    console.log(this.credentials);
 
     return this.http.post('https://test.midata.coop/v1/auth', this.credentials).map(
       (response) => response.json()).subscribe(
